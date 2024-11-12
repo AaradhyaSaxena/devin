62f05cf2835a77cf6e11eb3c6f917cdd0153d421
Aaradhya Saxena <aaradhya271197@gmail.com>
2024-11-12 17:12:36 +0530

    exclude/include file extensions

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import os
 from config import load_config
-from data_loader import analyze_repository
+from data_loader import RepositoryAnalyzer
 from llm_chain import LLMChain
 from code_generation_engine import CodeGenerationEngine
 from app import APP
@@ -8,7 +8,8 @@ from app import APP
 def main():
     load_config()
     
-    df = analyze_repository('./codebase/')
+    repo_analyzer = RepositoryAnalyzer()
+    df = repo_analyzer.analyze_repository('./codebase/')
     print(df.head())
     
     llm_chain = LLMChain(df)
